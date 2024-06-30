4adcc1ee807cbd3168666f79e4e60df67dd191bf
Kimhouy HOR <kimhouy.hor@allweb.com.kh>
2024-07-01 06:31:33 +0700

    font 16px to prevent zoom on iphone

--- components/Selection.tsx
+++ components/Selection.tsx
@@ -4,7 +4,7 @@ import { Field, Label, Select } from '@headlessui/react'
 const Selection = (props: {option: userRoleType[], name: string, label: string, selectedString: string}) => {
   return (
     <Field>
-        <Label htmlFor={props.name} className="block text-sm font-medium leading-6 ">{props.label}</Label>
+        <Label htmlFor={props.name} className="block text-base font-medium leading-6 ">{props.label}</Label>
         <div className="mt-2">
             <Select name={props.name} id={props.name} defaultValue={props.selectedString}  className="block w-full h-10 rounded-md border-0 dark:bg-slate-600 px-2 py-1.5 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6">
                 {
